6bc64b2c00f9ddda52fa5559ddcd86acee765565
Akash <akashad@live.co.uk>
2022-01-29 18:05:22 -0500

    DONE

---
+++ KitchenApp/Common/Extensions/Extension+UIButton.swift
@@ -0,0 +1,25 @@
+//
+//  Extension+UIButton.swift
+//  KitchenApp
+//
+//  Created by Akash Desai on 2022-01-29.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+    func midnightBlueRoundedButton() {
+        let image = UIImage(color: Asset.Colors.midnightBlue.color, size: CGSize(width: 1.0, height: 1.0))
+        let disabledColor = Asset.Colors.midnightBlue.color.withAlphaComponent(0.7)
+        let disabledImage = UIImage(color: disabledColor, size: CGSize(width: 1.0, height: 1.0))
+        self.setBackgroundImage(image, for: .normal)
+        self.setBackgroundImage(disabledImage, for: .disabled)
+        self.clipsToBounds = true
+        self.titleLabel?.font = UIFont(name: "HelveticaNeue-Medium", size: 18.0)
+        self.layer.cornerRadius = 8
+        self.tintColor = Asset.Colors.cloudsWhite.color
+    }
+    
+}
---
+++ KitchenApp/Common/Extensions/Extenstion+UIView.swift
@@ -0,0 +1,8 @@
+//
+//  Extenstion+UIView.swift
+//  KitchenApp
+//
+//  Created by Akash Desai on 2022-01-29.
+//
+
+import Foundation
